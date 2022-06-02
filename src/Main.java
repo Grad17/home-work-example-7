cf4cb5d2c58e2eb1a675dd80baa90e1f115c83bf
serge <sergei.g.d@gmail.ccom>
2022-06-02 20:02:55 +0700

    Сделано.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        String firstName = ("Ivan");
+        String middleName = ("Ivanovich");
+        String lastName = ("Ivanov");
+        String fullName = ("Ivanov Ivan Ivanovich");
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        String fullNameToUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameToUpperCase);
+
+        String fullName2 = fullName.replace("Ivanov Ivan Ivanovich","Иванов Семён Семёнович");
+        if (fullName2.contains("ё")) {
+            String e = fullName2.replace('ё', 'е');
+            System.out.println("Данные ФИО сотрудника (исправлены) — " + e);
+        } else System.out.println("Данные ФИО сотрудника — " + fullName2);
+
+
+//        String [] words = fullName2.split(" ");
+//
+//        System.out.println(words [0] + " " + words [1] + " " + words [2]);
+
     }
 }
